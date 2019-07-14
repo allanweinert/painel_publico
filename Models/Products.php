ef04d0c013269735931bfcbc0207b73b32819d35
allan.weinert <allan_weinert@hotmail.com>
2019-07-14 18:47:40 -0300

    Ajuste na inserção de imagens no produto, ainda com erro ao inserir no bd

--- Models/Products.php
+++ Models/Products.php
@@ -7,26 +7,26 @@ class Products extends Model {
 
 	public function getAll() {
 		$array = array();
+
 		$cat = new Categories();
 		$brands = new Brands();
 
 		$sql = "SELECT id, id_category, id_brand, name, stock, price, price_from FROM products";
 		$sql = $this->db->query($sql);
-		
 
 		if($sql->rowCount() > 0) {
 			$array = $sql->fetchAll(\PDO::FETCH_ASSOC);
 
-			foreach ($array as $key => $item) {
+			foreach($array as $key => $item) {
 				$catInfo = $cat->get($item['id_category']);
 				$brandInfo = $brands->get($item['id_brand']);
 
-				$array[$key] ['name_category'] = $catInfo['name'];
-				$array[$key] ['name_brand'] = $brandInfo['name'];
+				$array[$key]['name_category'] = $catInfo['name'];
+				$array[$key]['name_brand'] = $brandInfo['name'];
 			}
-
 		}
-			return $array;
+
+		return $array;
 	}
 
 	public function add($id_category,
@@ -62,8 +62,7 @@ class Products extends Model {
 
 			$options_ids = implode(',', array_keys($options_selected));
 
-			$sql = "INSERT INTO products (id_category, id_brand, name, description, stock, price, price_from, featured, sale, bestseller, new_product, options, weight, width, height, length, diameter) VALUES (:id_category, :id_brand, :name, :description, :stock, :price, :price_from, :featured, :sale, :bestseller, :new_product, :options, :weight, :width, :height, :length, :diameter)";
-			
+			$sql = "INSERT INTO products (id_category, id_brand, name, description, stock, price, price_from, featured, sale, bestseller, new_product, options, weight, width, height, length, diameter) VALUES (:id_category, :id_brand, :name, :description, :stock, :price, :price_from, :featured, :sale, :bestseller, :new_product, :options, :weight, :width, :height, :length, :diameter)";			
 			$sql = $this->db->prepare($sql);
 			$sql->bindValue(':id_category', $id_category);
 			$sql->bindValue(':id_brand', $id_brand);
@@ -86,7 +85,7 @@ class Products extends Model {
 
 			$id = $this->db->lastInsertId();
 
-			foreach ($options_selected as $optk => $opt) {
+			foreach($options_selected as $optk => $opt) {
 				
 				$sql = "INSERT INTO products_options (id_product, id_option, p_value) VALUES (:id_product, :id_option, :p_value)";
 				$sql = $this->db->prepare($sql);
@@ -98,38 +97,35 @@ class Products extends Model {
 			}
 
 			$allowed_images = array(
-				'images/jpeg',
-				'images/jpg',
-				'images/png'
+				'image/jpeg',
+				'image/jpg',
+				'image/png'
 			);
-
 			for($q=0;$q<count($images['name']);$q++) {
-				$tmp_name = $images['tmp_name'] [$q];
+
+				$tmp_name = $images['tmp_name'][$q];
 				$type = $images['type'][$q];
 
-				if (in_array($type, $allowed_images)) {
+				if(in_array($type, $allowed_images)) {
 
 					$this->addProductImage( $id, $tmp_name, $type );
 
-					echo "<pre>";
-			print_r($img);
-			exit;
-			echo "</pre>";
-
 				}
+
 			}
+
 		}
+
 	}
 
 	public function addProductImage($id, $tmp_name, $type) {
 
-		switch ($type) {
-			case 'images/jpeg':
-			case 'images/jpg':
+		switch($type) {
+			case 'image/jpeg':
+			case 'image/jpg':
 				$o_img = imagecreatefromjpeg($tmp_name);
-				break;
-			
-			case 'images/png':
+				break;			
+			case 'image/png':
 				$o_img = imagecreatefrompng($tmp_name);
 				break;
 		}
@@ -138,17 +134,17 @@ class Products extends Model {
 
 			$width = 460;
 			$height = 400;
-			$o_ratio = $width / $heigth;
+			$ratio = $width / $height;
 
-			list($o_width, $o_heigth) = getimagesize($tmp_name);
+			list($o_width, $o_height) = getimagesize($tmp_name);
 
-			$o_ratio = $o_width / $o_heigth;
+			$o_ratio = $o_width / $o_height;
 
 			if($ratio > $o_ratio) {
 				$img_w = $height * $o_ratio;
 				$img_h = $height;
 			} else {
-				$img_h = $width * $o_ratio;
+				$img_h = $width / $o_ratio;
 				$img_w = $width;
 			}
 
@@ -156,9 +152,9 @@ class Products extends Model {
 				$img_w = $width;
 				$img_h = $img_w / $o_ratio;
 			}
-			if($img_w < $width) {
+			if($img_h < $height) {
 				$img_h = $height;
-				$img_w = $img_h / $o_ratio;
+				$img_w = $img_h * $o_ratio;
 			}
 
 			$px = 0;
@@ -167,52 +163,27 @@ class Products extends Model {
 			if($img_w > $width) {
 				$px = ($img_w - $width) / 2;
 			}
+
 			if($img_h > $height) {
 				$py = ($img_h - $height) / 2;
 			}
 
-			$img = imagecreatetruecolor($width, $height);
-			imagecopyresampled($img, $o_img, 0, 0, 0, 0, $img_w, $img_h, $o_width, $o_heigth);
+
+			$img = imagecreatetruecolor( $width, $height );
+			imagecopyresampled($img, $o_img, -$px, -$py, 0, 0, $img_w, $img_h, $o_width, $o_height);
 
 			$filename = md5(time().rand(0,999).rand(0,999)).'.jpg';
 
 			imagejpeg($img, '../loja/media/products/'.$filename);
 
-
-		$sql = "INSERT INTO products_images (id_product, url) VALUES (:id_product, :url)";
+			$sql = "INSERT INTO products_images (id_product, url) VALUES (:id_product, :url)";
 			$sql = $this->db->prepare($sql);
 			$sql->bindValue(':id_product', $id);
-			$sql->bindValue(':url', $url);
+			$sql->bindValue(':url', $filename);
 			$sql->execute();
-
-			
 			
 		}
 
-
 	}
-
-/*	public function del ($id) {
-
-		$sql = "SELECT count(*) as c FROM products WHERE id_brand = id";
-		$sql = $this->db->prepare($sql);
-		$sql->bindValue(':id', $id);
-		$sql->execute();
-		$data = $sql->fetch();
-
-		if($data['c'] == '0') {
-			$sql = "DELETE FROM brands WHERE id = :id";
-			$sql = $this->db->prepare($sql);
-			$sql->bindValue(':id', $id);
-			$sql->execute();
-		}
-	}
-
-	public function update($name, $id) {
-		$sql = "UPDATE brands SET name = :name WHERE id = :id";
-		$sql = $this->db->prepare($sql);
-		$sql->bindValue(':name', $name);
-		$sql->bindValue(':id', $id);
-		$sql->execute();
-	}*/
+	
 }
